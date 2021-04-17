1b0063544b16325aa289aabe35d7e46a131aaa10
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2021-04-17 22:04:05 +0200

    Add a donation button

--- app.py
+++ app.py
@@ -86,6 +86,17 @@ def main():
                 .render_embed()  # generate a local HTML file
             )
             components.html(c, width=900, height=550)
+    
+    with st.sidebar:
+        st.markdown("---")
+        st.markdown(
+            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            unsafe_allow_html=True
+        )
 
 
 def render_basic_line():
