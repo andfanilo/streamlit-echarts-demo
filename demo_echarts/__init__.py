afcb6e9fd4b0acd7ed200df9b9ab14d237591ba2
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2021-05-22 12:40:21 +0200

    Finish echarts demo refactor

--- demo_echarts/__init__.py
+++ demo_echarts/__init__.py
@@ -1,20 +1,24 @@
 from .bar import ST_BAR_DEMOS
-from .line import ST_LINE_DEMOS
-from .pie import ST_PIE_DEMOS
-from .scatter import ST_SCATTER_DEMOS
-from .map import ST_MAP_DEMOS
-from .candlestick import ST_CANDLESTICK_DEMOS
-from .radar import ST_RADAR_DEMOS
 from .boxplot import ST_BOXPLOT_DEMOS
-from .heatmap import ST_HEATMAP_DEMOS
-from .graph import ST_GRAPH_DEMOS
-from .tree import ST_TREE_DEMOS
-from .treemap import ST_TREEMAP_DEMOS
-from .sunburst import ST_SUNBURST_DEMOS
-from .parallel import ST_PARALLEL_DEMOS
-from .sankey import ST_SANKEY_DEMOS
+from .calendar import ST_CALENDAR_DEMOS
+from .candlestick import ST_CANDLESTICK_DEMOS
+from .dataset import ST_DATASET_DEMOS
 from .funnel import ST_FUNNEL_DEMOS
 from .gauge import ST_GAUGE_DEMOS
+from .graph import ST_GRAPH_DEMOS
+from .heatmap import ST_HEATMAP_DEMOS
+from .line import ST_LINE_DEMOS
+from .map import ST_MAP_DEMOS
+from .parallel import ST_PARALLEL_DEMOS
+from .pictorial_bar import ST_PICTORIAL_BAR_DEMOS
+from .pie import ST_PIE_DEMOS
+from .radar import ST_RADAR_DEMOS
+from .sankey import ST_SANKEY_DEMOS
+from .scatter import ST_SCATTER_DEMOS
+from .sunburst import ST_SUNBURST_DEMOS
+from .themeriver import ST_THEMERIVER_DEMOS
+from .tree import ST_TREE_DEMOS
+from .treemap import ST_TREEMAP_DEMOS
 
 ST_DEMOS = {
     **ST_LINE_DEMOS,
@@ -34,4 +38,8 @@ ST_DEMOS = {
     **ST_SANKEY_DEMOS,
     **ST_FUNNEL_DEMOS,
     **ST_GAUGE_DEMOS,
+    **ST_PICTORIAL_BAR_DEMOS,
+    **ST_THEMERIVER_DEMOS,
+    **ST_CALENDAR_DEMOS,
+    **ST_DATASET_DEMOS,
 }
